99bbccc5f4959ce466d86bb33cde84882abc505c
Farzana2014 <farzana25sm@gmail.com>
2020-08-13 22:08:50 +0600

    Initial Commit

---
+++ CodeTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodeTest
+//
+//  Created by Badhon Jain on 8/13/20.
+//  Copyright © 2020 Self. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
