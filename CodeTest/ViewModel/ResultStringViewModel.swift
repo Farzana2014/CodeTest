8cc2c933446852fbb9bd1f5d6d5d3cdecf10902c
Farzana2014 <farzana25sm@gmail.com>
2020-08-15 22:20:46 +0600

    Method for last character, 10th character and wordlist implemented

--- CodeTest/ViewModel/ResultStringViewModel.swift
+++ CodeTest/ViewModel/ResultStringViewModel.swift
@@ -12,6 +12,8 @@ class ResultStringViewModel: ObservableObject {
     @Published var resultStrings = ResultString(requiredString: "")
     @Published var loading = false
 
+    var processingString = ""
+    
     let service: ServiceProtocol
     init(service: ServiceProtocol = APIService()) {
         self.service = service
@@ -24,7 +26,39 @@ class ResultStringViewModel: ObservableObject {
             guard let allString = allString else {
                 return
             }
-            self.resultStrings.requiredString = allString
+           // self.processingString = allString.trimmingCharacters(in: .whitespacesAndNewlines)
+           self.processingString = self.getEvery10thCharacter(string:allString)
+           self.processingString = self.processingString.appending(self.getLastCharacter(string:allString))
+           self.processingString = self.processingString.appending(self.getWordList(string:allString))
+           self.resultStrings.requiredString = self.processingString
         }
+    }
+    
+    func getEvery10thCharacter(string: String) -> String {
+        let array = String(string)
+        var appendedString = "Every 10th Character: \n \n"
+        
+        array.enumerated().forEach {
+            if( $0 % 10 == 0 && $0 > 0)
+            {print ($0, ":", $1)
+                appendedString = appendedString.appending(String($1).appending("\n"))
+            } else {  return}
+        }
+        return appendedString
+    }
+   
+    func getLastCharacter(string: String) -> String {
+        return "\n\n Last Character: \n\n , \(String(describing: string.last))"
+    }
+       
+    func getWordList(string: String) -> String {
+        
+        var appendedString = "\n\n Word List: \n\n"
+        
+        let wordList = string.split(separator: " ")
+        wordList.enumerated().forEach {
+            appendedString = appendedString.appending(String($0.element).appending("\n"))
+        }
+        return appendedString
     }
 }
